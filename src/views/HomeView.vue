d16e993bca85791687a286e6eb43ca8beef5b581
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-31 14:18:23 +0100

    feat: add functions to buttons on click

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
   <div class="">
     <div class="text-6xl m-10">{{ count }}</div>
     <div class="text-4xl flex justify-center space-x-4">
-      <button class="bg-gray-50 rounded border border-gray-200 px-2.5 py-2">-</button>
-      <button class="bg-gray-50 rounded border border-gray-200 p-2">+</button>
+      <button @click="decreaseCount" class="bg-gray-50 rounded border border-gray-200 px-2.5 py-2">-</button>
+      <button @click="increaseCount" class="bg-gray-50 rounded border border-gray-200 p-2">+</button>
     </div>
   </div>
 </template>
@@ -12,4 +12,12 @@
 import { ref } from 'vue'
 
 const count = ref(0)
+
+function increaseCount() {
+  count.value++
+}
+
+function decreaseCount() {
+  count.value--
+}
 </script>
