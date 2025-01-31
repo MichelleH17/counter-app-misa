c8c9e2e392004e2584f9a1d08c6c9cc92f2efcf7
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-31 14:33:25 +0100

    feat: add functionality to check  if number is odd or even

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,15 @@
       <button @click="decreaseCount" class="bg-gray-50 rounded border border-gray-200 px-2.5 py-2">-</button>
       <button @click="increaseCount" class="bg-gray-50 rounded border border-gray-200 p-2">+</button>
     </div>
+    <hr class="my-10 max-w-md mx-auto">
+    <div>
+      The counter is: {{ oddOrEven }}
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const count = ref(0)
 
@@ -20,4 +24,9 @@ function increaseCount() {
 function decreaseCount() {
   count.value--
 }
+
+const oddOrEven = computed(() => {
+  if (count.value % 2 === 0) return 'even'
+  return 'odd'
+})
 </script>
