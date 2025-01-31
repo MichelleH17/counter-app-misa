05d3e0dbe65653e8449b5ffc0ada3dc4fd9a8858
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-31 15:11:40 +0100

    feat: add button and use store on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h1 class="text-6xl m-10">About</h1>
+    <button @click="storeCounter.increaseCount" class="bg-gray-50 rounded border border-gray-200 px-2.5 py-2">
+      {{ storeCounter.count }}
+    </button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+
+const storeCounter = useCounterStore()
+</script>
+
