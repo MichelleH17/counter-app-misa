2a8d0611662480aebebb048e1d7fafe95f40a4c6
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-31 12:26:13 +0100

    feat: count with buttons and styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div class="">
+    <div class="text-6xl m-10">0</div>
+    <div class="text-4xl flex justify-center space-x-4">
+      <button class="bg-gray-50 rounded border border-gray-200 px-2.5 py-2">-</button>
+      <button class="bg-gray-50 rounded border border-gray-200 p-2">+</button>
+    </div>
   </div>
 </template>
