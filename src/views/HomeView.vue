756bc360421e71923c8a9e3b65fabc04ac67671f
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-31 12:30:34 +0100

    feat: add count ref and apply it

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="">
-    <div class="text-6xl m-10">0</div>
+    <div class="text-6xl m-10">{{ count }}</div>
     <div class="text-4xl flex justify-center space-x-4">
       <button class="bg-gray-50 rounded border border-gray-200 px-2.5 py-2">-</button>
       <button class="bg-gray-50 rounded border border-gray-200 p-2">+</button>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
